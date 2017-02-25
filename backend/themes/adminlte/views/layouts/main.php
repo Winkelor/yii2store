5f0d35f0d86f8d0a6cbeb60376fcba948dd4c6d9
Anton Beletsky <antonbeletskyeu@gmail.com>
2017-02-25 13:49:09 +0200

    admin panel

--- backend/themes/adminlte/views/layouts/main.php
+++ backend/themes/adminlte/views/layouts/main.php
@@ -9,7 +9,9 @@ use yii\widgets\Breadcrumbs;
 use common\widgets\Alert;
 
 //AppAsset::register($this);
-AdminlteAsset::register($this);
+$assets = AdminlteAsset::register($this);
+//var_dump($assets->baseUrl); base url before img url
+
 $this->beginPage();
 ?>
 <!DOCTYPE html>
@@ -96,7 +98,7 @@ desired effect
                                         <a href="#">
                                             <div class="pull-left">
                                                 <!-- User Image -->
-                                                <img src="adminlte/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                                                <img src="<?= $assets->baseUrl ?>/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                                             </div>
                                             <!-- Message title and timestamp -->
                                             <h4>
@@ -180,14 +182,14 @@ desired effect
                         <!-- Menu Toggle Button -->
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <!-- The user image in the navbar-->
-                            <img src="adminlte/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+                            <img src="<?= $assets->baseUrl ?>/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
                             <!-- hidden-xs hides the username on small devices so only the image appears. -->
                             <span class="hidden-xs">Alexander Pierce</span>
                         </a>
                         <ul class="dropdown-menu">
                             <!-- The user image in the menu -->
                             <li class="user-header">
-                                <img src="adminlte/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                                <img src="<?= $assets->baseUrl ?>/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                                 <p>
                                     Alexander Pierce - Web Developer
@@ -237,7 +239,7 @@ desired effect
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel">
                 <div class="pull-left image">
-                    <img src="adminlte/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                    <img src="<?= $assets->baseUrl ?>/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                 </div>
                 <div class="pull-left info">
                     <p>Alexander Pierce</p>
@@ -299,6 +301,7 @@ desired effect
         <section class="content">
 
             <!-- Your Page Content Here -->
+            base url: <?= $bundle->baseUrl ?>
             <?= $content ?>
 
         </section>
