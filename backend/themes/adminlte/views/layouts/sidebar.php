d89d68a00f693dc7d3aea9641b20fb4268cbbb7d
Anton Beletsky <antonbeletskyeu@gmail.com>
2017-02-26 15:44:15 +0200

    header

--- backend/themes/adminlte/views/layouts/sidebar.php
+++ backend/themes/adminlte/views/layouts/sidebar.php
@@ -40,11 +40,12 @@ use yii\helpers\Url;
         <ul class="sidebar-menu">
             <li class="header">Admin</li>
             <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="<?= Url::to(['/admin/default/index']) ?>"><i class="fa fa-link"></i> <span>Admin</span></a></li>
-            <li><a href="<?= Url::to(['/rbac/default/index']) ?>"><i class="fa fa-link"></i> <span>RBAC GUI</span></a></li>
-            <li><a href="<?= Url::to(['/admin/rbac']) ?>"><i class="fa fa-link"></i> <span>RBAC</span></a></li>
+            <li class="active"><a href="<?= Url::to(['/admin/default/index']) ?>"><i class="fa fa-folder"></i> <span>Admin</span></a></li>
+            <li><a href="<?= Url::to(['/rbac/default/index']) ?>"><i class="fa fa-folder"></i> <span>RBAC GUI</span></a></li>
+            <li><a href="<?= Url::to(['/admin/rbac']) ?>"><i class="fa fa-folder"></i> <span>RBAC</span></a></li>
+            <li><a href="https://almsaeedstudio.com/themes/AdminLTE/"><i class="fa fa-cubes"></i> <span>Admin LTE</span></a></li>
             <li class="treeview">
-                <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
+                <a href="#"><i class="fa fa-folder"></i> <span>Multilevel</span>
                     <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
