75ebcd3c353baaabce6f8cf738d78896ce9825ec
Anton Beletsky <antonbeletskyeu@gmail.com>
2017-02-27 22:05:59 +0200

    admin panel

--- backend/themes/adminlte/views/layouts/sidebar/admin_menu.php
+++ backend/themes/adminlte/views/layouts/sidebar/admin_menu.php
@@ -9,11 +9,7 @@ use yii\helpers\Url;
 <li><a href="<?= Url::to(['/gii']) ?>"><i class="fa fa-gg"></i> <span>gii</span></a></li>
 <li><a href="https://almsaeedstudio.com/themes/AdminLTE/"><i class="fa fa-cubes"></i> <span>Admin LTE</span></a></li>
 <li class="treeview">
-    <a href="#"><i class="fa fa-folder"></i> <span>Users</span>
-        <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-    </a>
+    <a href="#"><i class="fa fa-folder"></i> <span>Users</span><span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>
     <ul class="treeview-menu">
         <li><a href="#">Account types</a></li>
         <li><a href="#">Admins</a></li>
--- backend/themes/adminlte/views/layouts/sidebar/seller_menu.php
+++ backend/themes/adminlte/views/layouts/sidebar/seller_menu.php
@@ -3,7 +3,20 @@ use yii\helpers\Url;
 ?>
 <li class="header">Seller</li>
 <li><a href="<?= Url::to(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Orders</span></a></li>
-<li><a href="<?= Url::to(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Catalog</span></a></li>
+<li class="treeview">
+    <a href="<?= Url::to(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Catalog</span><span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>
+    <ul class="treeview-menu">
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Categories</a></li>
+        <li class="treeview">
+            <a href="<?= Url::to(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Attributes</span><span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>
+            <ul class="treeview-menu">
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Categories</a></li>
+            </ul>
+
+    </ul>
+</li>
 <li><a href="<?= Url::to(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Shipping</span></a></li>
 <li><a href="<?= Url::to(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Customers</span></a></li>
 <li><a href="<?= Url::to(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Analytics</span></a></li>
