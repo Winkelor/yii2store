3f10c3e0f5974fd5ef2fd1a3fd41d249bd912240
Anton Beletsky <antonbeletskyeu@gmail.com>
2017-03-05 16:10:31 +0200

    admin panel

---
+++ console/migrations/shops/m170305_140901_shops_seo.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170305_140901_shops_seo extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170305_140901_shops_seo cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
---
+++ console/migrations/shops/m170305_140930_shops_config.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170305_140930_shops_config extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170305_140930_shops_config cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
