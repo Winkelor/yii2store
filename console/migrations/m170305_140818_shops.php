699894c6f845345ad59a353c7b2a96bc2129f8e6
Anton Beletsky <antonbeletskyeu@gmail.com>
2017-03-05 17:30:44 +0200

    admin panel

--- console/migrations/m170305_140818_shops.php
+++ console/migrations/m170305_140818_shops.php
@@ -35,13 +35,13 @@ class m170305_140818_shops extends Migration
             'name' => Schema::TYPE_STRING,
             'short_name' => Schema::TYPE_STRING,
 
-            'main_user_id' => Schema::TYPE_INTEGER,
-            'legal_info_id' => Schema::TYPE_INTEGER,
-            'type_id' => Schema::TYPE_INTEGER,
-            'status_id' => Schema::TYPE_INTEGER,
+            'main_user_id' => Schema::TYPE_INTEGER, //backend user
+            'legal_info_id' => Schema::TYPE_INTEGER, //shops_legal_info table
+            'type_id' => Schema::TYPE_INTEGER, //shops_types table
+            'status_id' => Schema::TYPE_INTEGER, //shops_statuses table
 
-            'config_id' => Schema::TYPE_INTEGER,
-            'seo_config_id' => Schema::TYPE_INTEGER,
+            'config_id' => Schema::TYPE_INTEGER, //shops_config_table
+            'seo_config_id' => Schema::TYPE_INTEGER, //shops_seo_table or hust seo table
             'commerce_data' => Schema::TYPE_INTEGER,
 
             'created_at' => Schema::TYPE_INTEGER,
---
+++ console/migrations/m170305_152312_attributes_categories.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170305_152312_attributes_categories extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170305_152312_attributes_categories cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
