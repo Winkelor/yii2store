32bdcf45c688ea665b2f0d0a4db67c8475b12b7c
Anton Beletsky <antonbeletskyeu@gmail.com>
2017-03-08 03:38:03 +0200

    shops catalog

--- console/migrations/m170305_152112_products.php
+++ console/migrations/m170305_152112_products.php
@@ -13,6 +13,24 @@ class m170305_152112_products extends Migration
             'id' => Schema::TYPE_BIGPK,
             'shop_id' => Schema::TYPE_BIGPK,
 
+            'category_id' => Schema::TYPE_STRING,
+
+            // артикул
+            'vendor_code' => Schema::TYPE_STRING,
+            'name' => Schema::TYPE_STRING,
+            //короткий опис
+            'short_description' => Schema::TYPE_STRING,
+            //великий опис
+            'description' => Schema::TYPE_STRING,
+
+            /// img`s path
+            // обложка
+            'cover_img' => Schema::TYPE_STRING,
+            // мініатюра
+            'thumbnail_img' => Schema::TYPE_STRING,
+            // мініатюра меню
+            'thumbnails_img' => Schema::TYPE_STRING,
+
             'created_at' => Schema::TYPE_INTEGER,
             'updated_at' => Schema::TYPE_INTEGER,
         ], $tableOptions);
