13ddaa6f4f583fcec7b5348849abefc09994bea7
Anton Beletsky <antonbeletskyeu@gmail.com>
2017-03-15 14:27:54 +0200

    shop

---
+++ console/migrations/m170305_140816_shop_statuses.php
@@ -0,0 +1,46 @@
+<?php
+//ця таблиця описує магазини
+
+use yii\db\Migration;
+use yii\db\Schema; // http://www.yiiframework.com/doc-2.0/yii-db-schema.html
+
+class m170305_140816_shop_statuses extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%shop_statuses}}', [
+            'id' => Schema::TYPE_BIGPK,
+            'name' => Schema::TYPE_STRING,
+
+            'is_active' => Schema::TYPE_BOOLEAN,
+
+            'created_at' => Schema::TYPE_INTEGER,
+            'updated_at' => Schema::TYPE_INTEGER,
+        ], $tableOptions);
+
+    }
+
+    public function down()
+    {
+        echo "m170305_140818_shops cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
