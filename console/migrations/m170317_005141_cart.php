cc8b60c0f390413190c84bf74a5a15206bf68cec
Anton Beletsky <antonbeletskyeu@gmail.com>
2017-03-17 03:01:56 +0200

    'products_attributes_logistics_info' => Schema::TYPE_BIGPK, переробтити на багато багато

---
+++ console/migrations/m170317_005141_cart.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170317_005141_cart extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170317_005141_cart cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
---
+++ console/migrations/m170317_005212_orders_details.php
@@ -0,0 +1,51 @@
+<?php
+
+use yii\db\Migration;
+
+class m170317_005212_orders_details extends Migration
+{
+    public function up()
+    {
+        $this->createTable('{{%_details}}', [
+            'id' => Schema::TYPE_BIGPK,
+            // магазин
+            'shop_id' => Schema::TYPE_BIGPK,
+            // відділ магазину
+            'department_id' => Schema::TYPE_BIGPK, /* optional */
+            // товар
+            'product_id' => Schema::TYPE_BIGPK,
+            // атрибут товару (логістики)
+            'products_attributes_logistics_info' => Schema::TYPE_BIGPK,
+            // ціна для покупця
+            'price' => Schema::TYPE_DECIMAL,
+            // кількість
+            'count' => Schema::TYPE_INTEGER,
+            //статус
+            'status_id' =>  Schema::TYPE_INTEGER,
+
+            //покупець
+            'user_client_id' => Schema::TYPE_INTEGER,
+
+            'created_at' => Schema::TYPE_INTEGER,
+            'updated_at' => Schema::TYPE_INTEGER,
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        echo "m170317_005212_orders_details cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
