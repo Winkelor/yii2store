3034d05bc3e5a1631032fa53ad6ef3f40f27d3e7
Anton Beletsky <antonbeletskyeu@gmail.com>
2017-03-26 15:14:04 +0300

    langs

---
+++ console/migrations/m170326_121306_lang_translate_example.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+use yii\db\Schema;
+
+class m170326_121306_lang_translate_example extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170326_121306_lang_translate_example cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
