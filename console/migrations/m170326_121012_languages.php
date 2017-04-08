936a132c5b22fc85fa3864a1259cc6cf2accc55b
Anton Beletsky <antonbeletskyeu@gmail.com>
2017-04-08 19:48:04 +0300

    cultures

--- console/migrations/m170326_121012_languages.php
+++ console/migrations/m170326_121012_languages.php
@@ -3,7 +3,8 @@
 use yii\db\Migration;
 use yii\db\Schema;
 
-//see it https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+// see it https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+// see it https://www.w3schools.com/tags/ref_language_codes.asp
 class m170326_121012_languages extends Migration
 {
     public function up()
@@ -13,8 +14,10 @@ class m170326_121012_languages extends Migration
         $this->createTable('{{%languages}}', [
             'id' => Schema::TYPE_BIGPK,
             'en_name' => Schema::TYPE_STRING, // ukrainian
+            'iso639_1' => Schema::TYPE_STRING, //uk
             'native_name' => Schema::TYPE_STRING, //українська
-            'code' => Schema::TYPE_STRING,
+            'native_name_short' => Schema::TYPE_STRING, //укр
+
 
             'created_at' => Schema::TYPE_INTEGER,
             'updated_at' => Schema::TYPE_INTEGER,
