e74fb2a3947f5ddaafbe5339e2d19e4346bbbb42
Anton Beletsky <antonbeletskyeu@gmail.com>
2017-04-10 17:15:56 +0300

    фікс роутів

--- console/migrations/m180310_154800_foreign_keys_for_shops.php
+++ console/migrations/m180310_154800_foreign_keys_for_shops.php
@@ -8,7 +8,45 @@ class m180310_154800_foreign_keys_for_shops extends Migration
 {
     public function up()
     {
+        $Indexes = [
+            'm170227_134053_user_client' => [
+                'idx_user_client_account_type', // index name
+                '{{%user_client}}', // table name
+                'account_type_id' // column name
+            ],
+            1 => [
 
+            ],
+        ];
+
+        $Foreign_keys = [
+            'm170227_134053_user_client' => [
+                'fk_user_client_account_type',
+                '{{%user_client}}',
+                'account_type_id',
+                'usr_accounts_types',
+                'id',
+                'CASCADE'
+            ],
+            1 => [
+
+            ],
+        ];
+
+//        $this->createIndex(
+//            'idx_user_client_account_type',
+//            '{{%user_client}}',
+//            'account_type_id'
+//        );
+//
+//        $this->addForeignKey(
+//            'fk_user_client_account_type',
+//            '{{%user_client}}',
+//            'account_type_id',
+//            'usr_accounts_types',
+//            'id',
+//            'CASCADE'
+//        );
     }
 
     public function down()
