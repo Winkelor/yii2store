2d83418fab1f2b3d866a77448e3dbd497a475809
Anton Beletsky <antonbeletskyeu@gmail.com>
2017-04-16 14:13:15 +0300

    jr

--- backend/modules/admin/views/default/index.php
+++ backend/modules/admin/views/default/index.php
@@ -1,6 +1,7 @@
 <div class="admin-default-index">
     <h1><?= $this->context->action->uniqueId ?></h1>
     <p>
+        123
         This is the view content for action "<?= $this->context->action->id ?>".
         The action belongs to the controller "<?= get_class($this->context) ?>"
         in the "<?= $this->context->module->id ?>" module.
