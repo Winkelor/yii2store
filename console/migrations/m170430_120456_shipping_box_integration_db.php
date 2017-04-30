db3374b6e2153a17f21e897effbe4fa9f296f697
AntonBeletsky <antonbeletskyeu@gmail.com>
2017-04-30 16:59:54 +0300

    make shipping box

--- console/migrations/m170430_120456_shipping_box_integration_db.php
+++ console/migrations/m170430_120456_shipping_box_integration_db.php
@@ -6,9 +6,43 @@ use yii\db\Schema;
 // ця міграція інтегрує shipping box в базу, ставить вторичні ключі, індекси, додає значення
 class m170430_120456_shipping_box_integration_db extends Migration
 {
+    public $keys = [
+        [
+            'table_name' => '{{%shops_cultures}}',
+            'column_name' => 'culture_id',
+            'other_table_name' => '{{%cultures}}',
+        ],
+
+    ];
+
     public function up()
     {
 
+        foreach ($this->keys as $k => $key)
+        {
+            $key['table_name'] = str_replace("{{%", "", $key['table_name']);
+            $key['table_name'] = str_replace("}}", "", $key['table_name']);
+
+            $column = str_replace("_id", "", $key['column_name']);
+            $key['index_name'] = "idx_{$key['table_name']}__{$column}";
+            $key['foreign_key_name'] = "fk_{$key['table_name']}__{$column}";
+
+            $this->createIndex(
+                $key['index_name'],
+                $key['table_name'],
+                $key['column_name']
+            );
+            
+            $this->addForeignKey(
+                $key['foreign_key_name'],
+                $key['table_name'],
+                $key['column_name'],
+                $key['other_table_name'],
+                'id',
+                'CASCADE',
+                'CASCADE'
+            );
+        }
     }
 
     public function down()
