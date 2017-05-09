46644bb7dd0c78983c5186eea40ae69b5098a7c1
AntonBeletsky <antonbeletskyeu@gmail.com>
2017-05-09 17:03:40 +0300

    add shops to admin panel

--- backend/modules/admin/modules/shopsadministration/controllers/DefaultController.php
+++ backend/modules/admin/modules/shopsadministration/controllers/DefaultController.php
@@ -3,11 +3,12 @@
 namespace backend\modules\admin\modules\shopsadministration\controllers;
 
 use yii\web\Controller;
+use backend\controllers\BaseController;
 
 /**
  * Default controller for the `shopsadministration` module
  */
-class DefaultController extends Controller
+class DefaultController extends BaseController
 {
     /**
      * Renders the index view for the module
