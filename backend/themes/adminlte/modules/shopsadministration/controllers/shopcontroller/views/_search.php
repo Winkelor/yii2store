c65a0d4d79bff55c70ec80772b797c26eba534e4
AntonBeletsky <antonbeletskyeu@gmail.com>
2017-05-09 17:06:45 +0300

    shops crud

---
+++ backend/themes/adminlte/modules/shopsadministration/controllers/shopcontroller/views/_search.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\ShopsSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="shops-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'short_name') ?>
+
+    <?= $form->field($model, 'main_user_id') ?>
+
+    <?= $form->field($model, 'type_id') ?>
+
+    <?php // echo $form->field($model, 'status_id') ?>
+
+    <?php // echo $form->field($model, 'address_id') ?>
+
+    <?php // echo $form->field($model, 'contact_id') ?>
+
+    <?php // echo $form->field($model, 'created_at') ?>
+
+    <?php // echo $form->field($model, 'updated_at') ?>
+
+    <?php // echo $form->field($model, 'country_id') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('shops', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('shops', 'Reset'), ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ common/models/ShopsSearch.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Shops;
+
+/**
+ * ShopsSearch represents the model behind the search form about `common\models\Shops`.
+ */
+class ShopsSearch extends Shops
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'main_user_id', 'type_id', 'status_id', 'address_id', 'contact_id', 'created_at', 'updated_at', 'country_id'], 'integer'],
+            [['name', 'short_name'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Shops::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'main_user_id' => $this->main_user_id,
+            'type_id' => $this->type_id,
+            'status_id' => $this->status_id,
+            'address_id' => $this->address_id,
+            'contact_id' => $this->contact_id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'country_id' => $this->country_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'short_name', $this->short_name]);
+
+        return $dataProvider;
+    }
+}
