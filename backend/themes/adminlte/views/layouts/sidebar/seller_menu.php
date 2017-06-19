774489b87576452fd2173388382498f8a07205d8
Anton Beletsky <antonbeletskyeu@gmail.com>
2017-06-19 20:38:31 +0300

    frontend India edition

--- backend/themes/adminlte/views/layouts/sidebar/seller_menu.php
+++ backend/themes/adminlte/views/layouts/sidebar/seller_menu.php
@@ -18,31 +18,31 @@ $shop_id = ($shop != null)? $shop->id : '';
         </li>
     </ul>
 </li>
-<li><a href="<?= Url::toRoute(['/admin', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),]) ?>"><i class="fa fa-folder"></i> <span>Orders</span></a></li>
+<li><a class="leftmenu" href="<?= Url::toRoute(['/admin', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),]) ?>"><i class="fa fa-folder"></i> <span>Orders</span></a></li>
 <li class="treeview">
     <a href="<?= Url::toRoute(['/seller/catalog', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),]) ?>"><i class="fa fa-folder"></i> <span>Catalog</span><span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>
     <ul class="treeview-menu">
-        <li><a href="<?= Url::toRoute(['/seller/catalog/products', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),]) ?>">Products</a></li>
-        <li><a href="<?= Url::toRoute(['/seller/catalog/categories', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),]) ?>">Categories</a></li>
+        <li><a class="leftmenu" href="<?= Url::toRoute(['/seller/catalog/products', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),]) ?>">Products</a></li>
+        <li><a class="leftmenu" href="<?= Url::toRoute(['/seller/catalog/categories', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),]) ?>">Categories</a></li>
         <li class="treeview">
             <a href="<?= Url::toRoute(['/seller/catalog/attributes', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),])?>"><i class="fa fa-folder"></i> <span>Attributes</span><span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>
             <ul class="treeview-menu">
-                <li><a href="<?= Url::toRoute(['/seller/catalog/attributes/attributesproducts', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),])?>">Products</a></li>
-                <li><a href="<?= Url::toRoute(['/seller/catalog/attributes/attributescategories', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),])?>">Categories</a></li>
-                <li><a href="<?= Url::toRoute(['/seller/catalog/attributes/attributesgroups', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),])?>">Groups</a></li>
-                <li><a href="<?= Url::toRoute(['/seller/catalog/attributes/attributesproductsgroup', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),])?>">Products Group</a></li>
-                <li><a href="<?= Url::toRoute(['/seller/catalog/attributes/attributestypes', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),])?>">Types</a></li>
-                <li><a href="<?= Url::toRoute(['/seller/catalog/attributes/productsattributeslogisticsinfo', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),])?>">productsattributeslogisticsinfo</a></li>
-                <li><a href="<?= Url::toRoute(['/seller/catalog/attributes/productsattributeslogisticsinfostatuses', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),])?>">productsattributeslogisticsinfostatuses</a></li>
+                <li><a class="leftmenu" href="<?= Url::toRoute(['/seller/catalog/attributes/attributesproducts', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),])?>">Products</a></li>
+                <li><a class="leftmenu" href="<?= Url::toRoute(['/seller/catalog/attributes/attributescategories', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),])?>">Categories</a></li>
+                <li><a class="leftmenu" href="<?= Url::toRoute(['/seller/catalog/attributes/attributesgroups', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),])?>">Groups</a></li>
+                <li><a class="leftmenu" href="<?= Url::toRoute(['/seller/catalog/attributes/attributesproductsgroup', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),])?>">Products Group</a></li>
+                <li><a class="leftmenu" href="<?= Url::toRoute(['/seller/catalog/attributes/attributestypes', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),])?>">Types</a></li>
+                <li><a class="leftmenu" href="<?= Url::toRoute(['/seller/catalog/attributes/productsattributeslogisticsinfo', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),])?>">productsattributeslogisticsinfo</a></li>
+                <li><a class="leftmenu" href="<?= Url::toRoute(['/seller/catalog/attributes/productsattributeslogisticsinfostatuses', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),])?>">productsattributeslogisticsinfostatuses</a></li>
             </ul>
     </ul>
 </li>
-<li><a href="<?= Url::toRoute(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Shipping</span></a></li>
-<li><a href="<?= Url::toRoute(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Customers</span></a></li>
-<li><a href="<?= Url::toRoute(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Analytics</span></a></li>
-<li><a href="<?= Url::toRoute(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Managers</span></a></li>
-<?php /*  срань: <li><a href="<?= Url::base() . '/' . "uk-UA" . '/site/index' ?>"><i class="fa fa-folder"></i> <span>LALAKA</span></a></li> */ ?>
-<li><a href="<?= Url::toRoute(['/site/index', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),]) ?>"><i class="fa fa-folder"></i> <span>Site <?= Yii::$app->language ?></span></a></li>
-<li><a href="<?= Url::toRoute(['/site/index'])?>"><i class="fa fa-folder"></i> <span>My Helper Site <?= Yii::$app->language ?></span></a></li>
-<li><a href="<?= Url::toRoute(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Kurwa </span></a></li>
+<li><a class="leftmenu" href="<?= Url::toRoute(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Shipping</span></a></li>
+<li><a class="leftmenu" href="<?= Url::toRoute(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Customers</span></a></li>
+<li><a class="leftmenu" href="<?= Url::toRoute(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Analytics</span></a></li>
+<li><a class="leftmenu" href="<?= Url::toRoute(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Managers</span></a></li>
+<?php /*  срань: <li><a class="leftmenu" href="<?= Url::base() . '/' . "uk-UA" . '/site/index' ?>"><i class="fa fa-folder"></i> <span>LALAKA</span></a></li> */ ?>
+<li><a class="leftmenu" href="<?= Url::toRoute(['/site/index', 'lang' => Yii::$app->request->get('lang'), 'country' => Yii::$app->request->get('country'),]) ?>"><i class="fa fa-folder"></i> <span>Site <?= Yii::$app->language ?></span></a></li>
+<li><a class="leftmenu" href="<?= Url::toRoute(['/site/index'])?>"><i class="fa fa-folder"></i> <span>My Helper Site <?= Yii::$app->language ?></span></a></li>
+<li><a class="leftmenu" href="<?= Url::toRoute(['/admin']) ?>"><i class="fa fa-folder"></i> <span>Kurwa </span></a></li>
 
--- backend/web/adminlte/js/menu.js
+++ backend/web/adminlte/js/menu.js
@@ -1,35 +1,20 @@
-//
-// /* $('a[href="/backend/web/en-us/seller/catalog"]').parent().addClass( "active" ); */
-//
-// var url = window.location.href;
-// var arr = $('a');
-//
-//
-//
-// var str = 'http://www.yandex.ru';
-//
-// if(str.indexOf('yandex.ru') + 1) {
-//
-// }
-//
-//
-// cur_url = 'seller/catalog/products';
-//
-// for(var i = 0; i < arr.length; i++)
-// {
-//     var aurl = $(arr[i])
-//         .attr('href')
-//         // .toString()
-//     ;
-//
-//     if(url.toString().indexOf(cur_url) + 1)
-//         $(arr[i]).parent().addClass( "active" );
-//
-//
-//
-//     // if(aurl.indexOf(cur_url)  + 1) {
-//     //     console.log(
-//     //         $(arr[i]).attr('href')
-//     //     );
-//     // }
-// }
+
+
+var a = $('a.leftmenu[href="' + window.location.pathname + '"]');
+
+    var url = $(a).attr('href');
+    if(url !== undefined)
+        if(url.length > 1)
+            if(url == window.location.pathname) {
+                // console.log("URL: " + url);
+                $(a).parent().addClass("active"); // целевое, продукт на пример
+                $(a).parent().parent().parent().addClass("active");
+                $(a).parent().parent().parent().parent().addClass("active");
+                $(a).parent().parent().parent().parent().parent().addClass("active");
+                $(a).parent().parent().parent().parent().parent().parent().addClass("active");
+                $(a).parent().parent().parent().parent().parent().parent().parent().addClass("active");
+                $(a).parent().parent().parent().parent().parent().parent().parent().parent().addClass("active");
+                $(a).parent().parent().parent().parent().parent().parent().parent().parent().parent().addClass("active");
+                $(a).parent().parent().parent().parent().parent().parent().parent().parent().parent().parent().addClass("active");
+                $(a).parent().parent().parent().parent().parent().parent().parent().parent().parent().parent().parent().addClass("active");
+            }
