7850376e1b8042b1b56016025333fba5cceedd57
Anton Beletsky <antonbeletskyeu@gmail.com>
2017-06-19 21:49:59 +0300

    frontend India edition

--- backend/web/adminlte/js/menu.js
+++ backend/web/adminlte/js/menu.js
@@ -1,26 +1,28 @@
-function rec(a) {
-    if(a.is('li') || a.is('ui') || a.is('a')) {
-        if(a.is('li'))
-        {
-            a.addClass("active");
-            console.log( a + 'ACTIVE');
-        }
-        rec(a.parent());
-    }
-    return a;
-}
+// function rec(a) {
+//     if(a.is('li') || a.is('ui') || a.is('a')) {
+//         if(a.is('li'))
+//         {
+//             a.addClass("active");
+//             console.log( a + 'ACTIVE');
+//         }
+//         rec(a.parent());
+//     }
+//     return a;
+// }
+//
+// console.log( window.location.pathname );
+// // rec($('a.leftmenu[href="' + window.location.pathname + '"]'));
 
-console.log( window.location.pathname );
-rec($('a.leftmenu[href="' + window.location.pathname + '"]'));
+var a = $('a.leftmenu[href="' + window.location.pathname + '"]');
 
 
-// $(a).parent().addClass("active"); // целевое, продукт на пример
-// $(a).parent().parent().parent().addClass("active");
-// $(a).parent().parent().parent().parent().addClass("active");
-// $(a).parent().parent().parent().parent().parent().addClass("active");
-// $(a).parent().parent().parent().parent().parent().parent().addClass("active");
-// $(a).parent().parent().parent().parent().parent().parent().parent().addClass("active");
-// $(a).parent().parent().parent().parent().parent().parent().parent().parent().addClass("active");
-// $(a).parent().parent().parent().parent().parent().parent().parent().parent().parent().addClass("active");
-// $(a).parent().parent().parent().parent().parent().parent().parent().parent().parent().parent().addClass("active");
-// $(a).parent().parent().parent().parent().parent().parent().parent().parent().parent().parent().parent().addClass("active");
+$(a).parent().addClass("active"); // целевое, продукт на пример
+$(a).parent().parent().parent().addClass("active");
+$(a).parent().parent().parent().parent().addClass("active");
+$(a).parent().parent().parent().parent().parent().addClass("active");
+$(a).parent().parent().parent().parent().parent().parent().addClass("active");
+$(a).parent().parent().parent().parent().parent().parent().parent().addClass("active");
+$(a).parent().parent().parent().parent().parent().parent().parent().parent().addClass("active");
+$(a).parent().parent().parent().parent().parent().parent().parent().parent().parent().addClass("active");
+$(a).parent().parent().parent().parent().parent().parent().parent().parent().parent().parent().addClass("active");
+$(a).parent().parent().parent().parent().parent().parent().parent().parent().parent().parent().parent().addClass("active");
